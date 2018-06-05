6b37f7c15d81b398e85686ebc4cd70c6a7dd2336
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-05 15:00:51 -0400

    Deleted unneeded method

--- task_screen.py
+++ task_screen.py
@@ -23,19 +23,3 @@ class Task_Screen():
 			else:
 				return ans
 				
-	def main_menu(self):
-		answers = ['a', 'b', 'c']
-	
-		print('WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\nc) Quit program')
-		while True:
-			ans = input('>')
-			try:
-				value = ans.lower()
-			except ValueError:
-				print('Please enter a valid letter.')
-				continue
-			if value in answers:
-				break
-			else:
-				print('Please enter a valid letter')
-				
