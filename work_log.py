4689e96b2c57ea26ef622f17fd2c13fd8547273e
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-05 12:51:59 -0400

    Created task file and prompted user for input

--- work_log.py
+++ work_log.py
@@ -1,1 +1,16 @@
 #Work log
+answers = ['a', 'b', 'c']
+
+print('WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\nc) Quit program')
+
+while True:
+	ans = input('>')
+	try:
+		value = ans.lower()
+	except ValueError:
+		print('Please enter a valid letter.')
+		continue
+	if value in answers:
+		break
+	else:
+		print('Please enter a valid letter')
