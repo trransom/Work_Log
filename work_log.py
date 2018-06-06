81256e0c9a2633f03540b5f2eea735deaedc7161
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-06 17:38:55 -0400

    Added comments to guide program sequence

--- work_log.py
+++ work_log.py
@@ -10,11 +10,20 @@ menu.display()
 inpt = menu.input()
 
 if inpt.lower()=='a':
+	#display the date task screen and retrieve the date.
 	diff = Task_Screen('Date of the task\nPlease use DD/MM/YYYY: ', '', '([0-3][0-9])\/([0-1][0-9])\/[0-9]{3}')
 	diff.display()
 	date = diff.input()
 	print(date)
-	#print('passed')
+	
+	#Retrieve the title of the task
+	
+	#Retrieve the time spent completing the task
+	
+	#Prompt for notes
+	
+	#Enter the task to the task log. Display a message prompting the user
+	#to press enter and to return to the main menu.
 elif inpt.lower()=='b':
 	pass
 elif inpt.lower()=='c':
