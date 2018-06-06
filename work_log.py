fb8e00cadc00a6227ecee66bd2b559a33238509d
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-06 17:35:07 -0400

    Changed year regex expression

--- work_log.py
+++ work_log.py
@@ -5,12 +5,12 @@ from task_screen import Task_Screen
 from task import Task
 
 t = 'WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\nc) Quit program'
-menu = Task_Screen(t, '>', 'a|b|c')
+menu = Task_Screen(t, '>', '[AaBbCc]')
 menu.display()
 inpt = menu.input()
 
 if inpt.lower()=='a':
-	diff = Task_Screen('Date of the task\nPlease use DD/MM/YYYY: ', '', '(\d{2}|\d{1})\/(\d{2}|\d{1})\/\d{4}')
+	diff = Task_Screen('Date of the task\nPlease use DD/MM/YYYY: ', '', '([0-3][0-9])\/([0-1][0-9])\/[0-9]{3}')
 	diff.display()
 	date = diff.input()
 	print(date)
