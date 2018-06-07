6de4db4904cb20c6d582870942aaf985f65860d7
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-07 14:33:26 -0400

    Changed utils to have a dictionary, deleted csv file

--- utils.py
+++ utils.py
@@ -1,1 +1,35 @@
 #Utils
+import csv
+
+class Utils():
+	
+	def __init__(self, dictionary={}):
+		self.dictionary = dictionary
+		
+	def print_dict(csv, num1, num2):
+		'''
+			Print all of the rows in the csv file by
+			converting them to a dictionary format.
+		'''
+		with open(csv, newline='') as csvfile:
+			dictreader = csv.DictReader(csvfile, delimter='|')
+			rows = list(dictreader)
+			for row in rows[num1:num2]:
+				print(row)
+				
+	def search_dict():
+		'''
+			
+		'''
+		pass
+		
+	def dates_list():
+		pass
+		
+	def log_task(task):
+		'''
+			Add a task to the log file.
+		'''
+		pass
+		
+	
