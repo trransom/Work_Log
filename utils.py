59e44c3a0dec56fab02f038abdf3b0b2d1646153
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-20 13:04:29 -0400

    Wrote docstrings for utils

--- utils.py
+++ utils.py
@@ -4,23 +4,35 @@ import csv
 class Utils():
 
 	def __init__(self, filename, format, fieldnames):
+		'''
+			Initializes a Utils object by declaring the
+			filename, the format, and the fieldnames.
+		'''
 		self.filename = filename
 		self.format = format
 		self.fieldnames = fieldnames
 
 	def write_header(self, filename, format, fieldnames):
+		'''
+			Writes a header to the specified file with the
+			given fieldnames.
+		'''
 		with open(filename, format) as csvfile:
 			self.writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 			self.writer.writeheader()
 		
 	def write_row(self, dictionary):
+		'''
+			Writes a row of data to the specified
+			csv file.
+		'''
 		with open(self.filename, 'a', newline='') as csvfile:
 			self.writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
 			self.writer.writerow(dictionary)
 		
 	def print_dict(csv, num1, num2):
 		'''
-			Print all of the rows in the csv file by
+			Prints all of the rows in the csv file by
 			converting them to a dictionary format.
 		'''
 		with open(csv, newline='') as csvfile:
@@ -29,19 +41,3 @@ class Utils():
 			for row in rows[num1:num2]:
 				print(row)
 				
-	def search_dict():
-		'''
-			
-		'''
-		pass
-		
-	def dates_list():
-		pass
-		
-	def log_task(task):
-		'''
-			Add a task to the log file.
-		'''
-		pass
-		
-	
