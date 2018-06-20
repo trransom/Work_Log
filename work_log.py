1de6e911010e36e5fe84312fe2f1c3da316ca98a
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-20 10:03:17 -0400

    Finalized the searching options

--- work_log.py
+++ work_log.py
@@ -10,11 +10,19 @@ from task import Task
 from utils import Utils
 
 def screen_prompt(display, input, regex):
+	'''
+		Displays a screen message and compares input
+		against a regex pattern.
+	'''
 	task = Task_Screen(display, input, regex)
 	task.display()
 	return task.input()
 	
 def task_display(num1, total):
+	'''
+		Displays a single task and allows the user
+		to cycle through a list of tasks.
+	'''
 	number = num1
 	print('Date: ' + list[number][0] + '\n' +
 			'Title: ' + list[number][1] + '\n' +
@@ -27,74 +35,127 @@ def task_display(num1, total):
 	else:
 		task_display(number, total)
 
-inpt = screen_prompt(
-			'WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\nc) Quit program', 
-			'>', 
-			'[AaBbCc]'
-			)
+def main():
+	inpt = screen_prompt(
+				'WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\nc) Quit program', 
+				'>', 
+				'[AaBbCc]'
+				)
 
-if inpt.lower()=='a':
-	#display the date task screen and retrieve the date.
-	date = screen_prompt('Date of the task\nPlease use MM/DD/YYYY: ', '', '([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}')
-	
-	#Retrieve the title of the task
-	name = screen_prompt('Name of the task: ', '>', '.*[\w\s].*')
-	
-	#Retrieve the time spent completing the task
-	time = screen_prompt('Time Spent (rounded by minute): ', '>', '\d+')
-	
-	#Prompt for notes
-	notes = screen_prompt('Notes (optional, allowed to leave blank): ', '>', '.*[\w\s].*')
-	
-	#Enter the task to the task log. Display a message prompting the user
-	task = Task(date, name, time, notes)
-	util = Utils('log.csv', 'a', ['date', 'name', 'time', 'notes'])
-	
-	#if log.csv is empty, write a header.
-	try:
-		if os.stat('log.csv').st_size == 0:
+	if inpt.lower()=='a':
+		#display the date task screen and retrieve the date.
+		date = screen_prompt('Date of the task\nPlease use MM/DD/YYYY: ', '', '([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}')
+		
+		#Retrieve the title of the task
+		name = screen_prompt('Name of the task: ', '>', '.*[\w\s].*')
+		
+		#Retrieve the time spent completing the task
+		time = screen_prompt('Time Spent (rounded by minute): ', '>', '\d+')
+		
+		#Prompt for notes
+		notes = screen_prompt('Notes (optional, allowed to leave blank): ', '>', '.*[\w\s].*')
+		
+		#Enter the task to the task log. Display a message prompting the user
+		task = Task(date, name, time, notes)
+		util = Utils('log.csv', 'a', ['date', 'name', 'time', 'notes'])
+		
+		#if log.csv is empty, write a header.
+		try:
+			if os.stat('log.csv').st_size == 0:
+				util.write_header(util.filename, util.format, util.fieldnames)
+		except FileNotFoundError:
 			util.write_header(util.filename, util.format, util.fieldnames)
-	except FileNotFoundError:
-		util.write_header(util.filename, util.format, util.fieldnames)
+			
+		util.write_row(task.dictionary)
 		
-	util.write_row(task.dictionary)
-	
-	#to press enter and to return to the main menu.
-elif inpt.lower()=='b':
-	options = screen_prompt('Do you want to search by:\na)Exact Date\n' +
-							'b)Range of Dates\nc)Exact Search\nd)Regex Pattern\n' +
-							'e)Return to Menu', '>', '[AaBbCcDdEe]')
-	if options.lower()=='a':
-		inpt = screen_prompt("Enter the date\nPlease use MM/DD/YYYY:", '>', '([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}')
-		csv_file = csv.reader(open('log.csv', 'r+'), delimiter=',')#encoding='utf-8' 
 		
-		list = []
-		for row in csv_file:
-			if len(row)==0:
-				continue
-			elif inpt == row[0]:
-				list.append(row)
+		#to press enter and to return to the main menu.
 		
-		task_display(0, len(list))
-		
-	elif options.lower()=='b':
-		inpt = screen_prompt('Enter the range of dates.\nPlease use MM/DD/YYYY, MM/DD/YYYY format',
-							'>', '([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}, ([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}')
-		csv_file = csv.reader(open('log.csv', 'r+'), delimiter=',')
-		
-		inpt = inpt.split(', ')
-		time1 = dt.strptime(inpt[0], '%m/%d/%Y')
-		time2 = dt.strptime(inpt[1], '%m/%d/%Y')
-		
-		list = []
-		for row in csv_file:
-			if len(row)==0 or row[0]=='date':
-				continue
-			#The datetime stamp here might give us errors down the road. Just FYI, keep an eye on it.
-			elif dt.strptime(row[0], '%m/%d/%Y') >= time1 and dt.strptime(row[0], '%m/%d/%Y') <= time2:
-				list.append(row)
-		task_display(0, len(list))
-		
+	#Prompt user for type of search wanted
+	elif inpt.lower()=='b':
+		options = screen_prompt('Do you want to search by:\na)Exact Date\n' +
+								'b)Range of Dates\nc)Exact Search\nd)Regex Pattern\n' +
+								'e)Return to Menu', '>', '[AaBbCcDdEe]')
+		#Prompt for date search.
+		if options.lower()=='a':
+			inpt = screen_prompt("Enter the date\nPlease use MM/DD/YYYY:", '>', '([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}')
+			csv_file = csv.reader(open('log.csv', 'r+'), delimiter=',')#encoding='utf-8' 
+			
+			list = []
+			for row in csv_file:
+				if len(row)==0:
+					continue
+				elif inpt == row[0]:
+					list.append(row)
+			
+			task_display(0, len(list))
+		#prompt for range of dates
+		elif options.lower()=='b':
+			inpt = screen_prompt('Enter the range of dates.\nPlease use MM/DD/YYYY, MM/DD/YYYY format',
+								'>', '([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}, ([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}')
+			csv_file = csv.reader(open('log.csv', 'r+'), delimiter=',')
+			
+			inpt = inpt.split(', ')
+			time1 = dt.strptime(inpt[0], '%m/%d/%Y')
+			time2 = dt.strptime(inpt[1], '%m/%d/%Y')
+			
+			list = []
+			for row in csv_file:
+				if len(row)==0 or row[0]=='date':
+					continue
+				#The datetime stamp here might give us errors down the road. Just FYI, keep an eye on it.
+				elif dt.strptime(row[0], '%m/%d/%Y') >= time1 and dt.strptime(row[0], '%m/%d/%Y') <= time2:
+					list.append(row)
+			
+			if list:
+				task_display(0, len(list))
+			else:
+				print('No matches found')
+				
+		#prompt for exact search
+		elif options.lower()=='c':
+			inpt = screen_prompt('Enter your exact search.\n', '>', '.*')
+			csv_file = csv.reader(open('log.csv', 'r+'), delimiter=',')
+			
+			list = []
+			for row in csv_file:
+				if len(row)==0:
+					continue
+				else:
+					for item in row:
+						if item==inpt:
+							list.append(row)
+							continue
+			if list:
+				task_display(0, len(list))
+			else:
+				print('No matches found')
+				
+		elif options.lower()=='d':
+			inpt = screen_prompt('Enter your regex pattern:\n', '>', '.*')
+			csv_file = csv.reader(open('log.csv', 'r+'), delimiter=',')
+			
+			list = []
+			for row in csv_file:
+				if len(row)==0 or row[0]=='date':
+					continue
+				else:
+					for item in row:
+						if re.search(inpt, item):
+							list.append(row)
+							break
+							
+			if list:
+				task_display(0, len(list))
+			else:
+				print('No matches found')
+			
+		elif options.lower()=='e':
+			main()
 
-elif inpt.lower()=='c':
-	pass
+	elif inpt.lower()=='c':
+		print('Thanks for using the Work Log program!')
+		sys.exit()
+	
+	
+main()
