d15339229c0059ae4e36b4266932413099a59954
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-20 13:01:08 -0400

    Added docstrings to task_screen

--- task_screen.py
+++ task_screen.py
@@ -5,16 +5,28 @@ import re
 class Task_Screen():
 
 	def __init__(self, display_message, input_prompt, answers):
+		'''
+			Initializes a Task_Screen object by setting
+			a display message, an input prompt, and a regex
+			pattern to compare answers against.
+		'''
 		self.display_message = display_message
 		self.input_prompt = input_prompt
 		self.answers = answers
 		
 	def display(self):
+		'''
+			Displays the message of the object.
+		'''
 		os.system('cls')
 		print(self.display_message)
 		
 
 	def input(self):
+		'''
+			Continues to prompt user for input until
+			the input matches the regex pattern.
+		'''
 		while True:
 			ans = input(self.input_prompt)
 			if not re.match(self.answers, ans):
